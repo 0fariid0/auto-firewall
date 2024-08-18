60d83ace2d755ac8afe5352bf18333c828907fe9
0fariid0 <126415296+0fariid0@users.noreply.github.com>
2024-08-18 20:44:19 +0330

    Update download_and_run.sh

--- download_and_run.sh
+++ download_and_run.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# آدرس فایل اسکریپت در GitHub
 GITHUB_REPO_URL="https://raw.githubusercontent.com/0fariid0/auto-firewall/main/ufw.sh"
 
-# دانلود اسکریپت از GitHub
 curl -O $GITHUB_REPO_URL
 
-# جدا کردن نام فایل از URL
 FILENAME=$(basename $GITHUB_REPO_URL)
 
-# دادن دسترسی اجرایی به اسکریپت
 chmod +x $FILENAME
 
-# اجرای اسکریپت
 sudo ./$FILENAME

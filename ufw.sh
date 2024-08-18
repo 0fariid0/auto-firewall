5e9c1b29e1211c51daf42d63cd89a875c933230b
0fariid0 <126415296+0fariid0@users.noreply.github.com>
2024-08-18 20:47:01 +0330

    Update ufw.sh

--- ufw.sh
+++ ufw.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-# چک کردن اینکه اسکریپت با دسترسی روت اجرا می‌شود
 if [[ $EUID -ne 0 ]]; then
-   echo "این اسکریپت باید با دسترسی روت اجرا شود." 
+   echo "This script must be run as root."
    exit 1
 fi
 
-# استخراج پورت‌های TCP که در حال LISTEN هستند
 ports=$(ss -tuln | grep LISTEN | awk '{print $5}' | awk -F':' '{print $NF}' | sort -n | uniq)
 
-# باز کردن پورت‌ها در ufw
 for port in $ports; do
-    echo "باز کردن پورت $port در ufw..."
     sudo ufw allow "$port"/tcp
 done
 
-# اضافه کردن قوانین اضافی به ufw
-echo "اضافه کردن قوانین اضافی به ufw..."
 sudo ufw deny out from any to 200.0.0.0/8
 sudo ufw deny out from any to 102.0.0.0/8
 sudo ufw deny out from any to 10.0.0.0/8
@@ -42,12 +36,8 @@ sudo ufw deny out from any to 102.230.9.0/24
 sudo ufw deny out from any to 102.233.71.0/24
 sudo ufw allow 222/tcp
 
-# فعال کردن ufw
-echo "فعال کردن ufw..."
-sudo ufw enable
+echo "y" | sudo ufw enable
 
-# اضافه کردن قوانین به iptables
-echo "اضافه کردن قوانین به iptables..."
 sudo iptables -A FORWARD -s 200.0.0.0/8 -j DROP
 sudo iptables -A FORWARD -s 102.0.0.0/8 -j DROP
 sudo iptables -A FORWARD -s 10.0.0.0/8 -j DROP
@@ -73,10 +63,6 @@ sudo iptables -A FORWARD -s 198.18.140.0/24 -j DROP
 sudo iptables -A FORWARD -s 102.230.9.0/24 -j DROP
 sudo iptables -A FORWARD -s 102.233.71.0/24 -j DROP
 
-# ذخیره کردن تنظیمات iptables
 sudo iptables-save
 
-# نمایش وضعیت ufw
 sudo ufw status verbose
-
-echo "پورت‌ها و قوانین فایروال با موفقیت اعمال شدند."

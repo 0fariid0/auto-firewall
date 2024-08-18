18516630ec6fbf9326108129968ab402d1eb1a89
0fariid0 <126415296+0fariid0@users.noreply.github.com>
2024-08-18 22:23:25 +0330

    Update download_and_run.sh

--- download_and_run.sh
+++ download_and_run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 GITHUB_REPO_URL="https://raw.githubusercontent.com/0fariid0/auto-firewall/main/ufw.sh"
+FILENAME=$(basename $GITHUB_REPO_URL)
+
+if [ -f "$FILENAME" ]; then
+    rm "$FILENAME"
+fi
 
 curl -O $GITHUB_REPO_URL
-
-FILENAME=$(basename $GITHUB_REPO_URL)
 
 chmod +x $FILENAME
 
